45f26681826d620c1d98f7e333ad21355d7e239f
Cloves José <clovesjsilva8@gmail.com>
2022-06-13 16:58:06 -0300

    transferência expo->react-native/navegacao

---
+++ frontEnd/src/data/ameacas.js
@@ -0,0 +1,68 @@
+export default [
+    {
+        id: 1,
+        title: 'Lixo urbano',
+        subtitle: 'O Lorem Ipsum é um texto modelo da indústria tipográfica e de impressão. O Lorem Ipsum tem vindo a ser o texto padrão usado por estas indústrias desde o ano de 1500.',
+        avatarUrl: 'https://cdn-icons.flaticon.com/png/512/484/premium/484662.png?token=exp=1655078019~hmac=6b63fc69ac12b89c817640a44131cd3a', 
+    },
+    {
+        id: 2,
+        title: 'Esgoto a céu aberto',
+        subtitle: 'O Lorem Ipsum é um texto modelo da indústria tipográfica e de impressão. O Lorem Ipsum tem vindo a ser o texto padrão usado por estas indústrias desde o ano de 1500.',
+        avatarUrl: 'https://cdn-icons-png.flaticon.com/512/3399/3399239.png',
+    },
+    {
+        id: 3,
+        title: 'Lixo biológico',
+        subtitle: 'O Lorem Ipsum é um texto modelo da indústria tipográfica e de impressão. O Lorem Ipsum tem vindo a ser o texto padrão usado por estas indústrias desde o ano de 1500.',
+        avatarUrl: 'https://cdn-icons.flaticon.com/png/512/4937/premium/4937541.png?token=exp=1655078093~hmac=394fe5ce72aa018a5df70b519744fe43',
+    },
+    {
+        id: 4,
+        title: 'Animais abandonados',
+        subtitle: 'O Lorem Ipsum é um texto modelo da indústria tipográfica e de impressão. O Lorem Ipsum tem vindo a ser o texto padrão usado por estas indústrias desde o ano de 1500.',
+        avatarUrl: 'https://cdn-icons.flaticon.com/png/512/1796/premium/1796997.png?token=exp=1655078164~hmac=216ecbfe7bcc46004f790f67a3d4a557',
+    },
+    {
+        id: 5,
+        title: 'Canos estourados',
+        subtitle: 'O Lorem Ipsum é um texto modelo da indústria tipográfica e de impressão. O Lorem Ipsum tem vindo a ser o texto padrão usado por estas indústrias desde o ano de 1500.',
+        avatarUrl: 'https://cdn-icons-png.flaticon.com/512/2302/2302749.png',
+    },
+    {
+        id: 6,
+        title: 'Quedas de arvores',
+        subtitle: 'O Lorem Ipsum é um texto modelo da indústria tipográfica e de impressão. O Lorem Ipsum tem vindo a ser o texto padrão usado por estas indústrias desde o ano de 1500.',
+        avatarUrl: 'https://cdn-icons-png.flaticon.com/512/2298/2298442.png',
+    },
+    {
+        id: 7,
+        title: 'Coleta seletiva',
+        subtitle: 'O Lorem Ipsum é um texto modelo da indústria tipográfica e de impressão. O Lorem Ipsum tem vindo a ser o texto padrão usado por estas indústrias desde o ano de 1500.',
+        avatarUrl: 'https://cdn-icons-png.flaticon.com/512/702/702626.png',
+    },
+    {
+        id: 8,
+        title: 'Resíduos de construção civil',
+        subtitle: 'O Lorem Ipsum é um texto modelo da indústria tipográfica e de impressão. O Lorem Ipsum tem vindo a ser o texto padrão usado por estas indústrias desde o ano de 1500.',
+        avatarUrl: 'https://cdn-icons-png.flaticon.com/512/1606/1606208.png',
+    },
+    {
+        id: 9,
+        title: 'Falta de saneamento básico',
+        subtitle: 'O Lorem Ipsum é um texto modelo da indústria tipográfica e de impressão. O Lorem Ipsum tem vindo a ser o texto padrão usado por estas indústrias desde o ano de 1500.',
+        avatarUrl: 'https://cdn-icons.flaticon.com/png/512/3216/premium/3216285.png?token=exp=1655078444~hmac=66a3851f421455b93dbc07fabb8378f5',
+    },
+    {
+        id: 10,
+        title: 'Falta de iluminação pública',
+        subtitle: 'O Lorem Ipsum é um texto modelo da indústria tipográfica e de impressão. O Lorem Ipsum tem vindo a ser o texto padrão usado por estas indústrias desde o ano de 1500.',
+        avatarUrl: 'https://cdn-icons-png.flaticon.com/512/2910/2910914.png',
+    },
+    {
+        id: 11,
+        title: 'Danos ao patrimônio público',
+        subtitle: 'O Lorem Ipsum é um texto modelo da indústria tipográfica e de impressão. O Lorem Ipsum tem vindo a ser o texto padrão usado por estas indústrias desde o ano de 1500.',
+        avatarUrl: 'https://cdn-icons.flaticon.com/png/512/4742/premium/4742318.png?token=exp=1655078530~hmac=aa90432e27d61465404296a233bbbd1c',
+    }
+]
