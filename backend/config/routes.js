2d3ef409d872212cc962477b7596cfa6462963cc
Cloves-Jose <64108214+Cloves-Jose@users.noreply.github.com>
2022-11-06 11:11:14 -0300

    Delete backend directory
    delecao backend

--- backend/config/routes.js
+++
@@ -1,4 +0,0 @@
-module.exports = app => {
-    app.post('/cadastrarAmeaca', app.api.controllers.ameacaController.salvarAmeaca)
-    app.get('/listarAmeaca', app.api.controllers.ameacaController.listarAmeaca)
-}
